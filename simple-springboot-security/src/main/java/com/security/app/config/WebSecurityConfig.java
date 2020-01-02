16f2d71863ef43d64128fb733273c031be1b4e68
nfebrian13 <nana.febriana@sinarmasmsiglife.co.id>
2020-01-02 15:18:22 +0700

    commit

--- simple-springboot-security/src/main/java/com/security/app/config/WebSecurityConfig.java
+++ simple-springboot-security/src/main/java/com/security/app/config/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.security.app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -28,8 +30,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .logout()
         .permitAll();
   }
+  
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+      auth.inMemoryAuthentication()
+          .withUser("user")
+          .password("{noop}password")
+          .roles("USER");
+  }
 
-  @Bean
+  
+/*  @Bean
   @Override
   public UserDetailsService userDetailsService() {
     UserDetails user = User.withDefaultPasswordEncoder()
@@ -39,6 +50,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .build();
 
     return new InMemoryUserDetailsManager(user);
-  }
+  } */
   
 }
---
+++ simple-springboot-security/src/main/java/com/security/app/controller/IndexController.java
@@ -0,0 +1,32 @@
+package com.security.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value = { "/login" }, method = RequestMethod.GET)
+	public ModelAndView login() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("login");
+		return modelAndView;
+	}
+
+	@RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
+	public ModelAndView home() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("home");
+		return modelAndView;
+	}
+
+	@RequestMapping(value = { "/hello" }, method = RequestMethod.GET)
+	public ModelAndView hello() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("hello");
+		return modelAndView;
+	}
+
+}
